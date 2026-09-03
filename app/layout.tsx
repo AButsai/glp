60d52cdb8865458db2364038a6bc41b9cb158120
AButsai <butsai@ukr.net>
2026-09-03 15:18:05 +0300

    gtm-script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/ui/sections/Footer'
 import Header from '@/ui/sections/Header'
 import type { Metadata } from 'next'
 import { DM_Serif_Display, Inter } from 'next/font/google'
+import Script from 'next/script'
 
 import {
 	itemsTopHeader,
@@ -34,7 +35,22 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
 			className={`${serif.variable} ${sans.variable} h-full scroll-smooth antialiased`}
 			data-scroll-behavior='smooth'
 		>
+			<Script id='gtm-script' strategy='afterInteractive'>
+				{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-NCWKWCPL');`}
+			</Script>
 			<body className='flex min-h-full flex-col'>
+				<noscript>
+					<iframe
+						src='https://www.googletagmanager.com/ns.html?id=GTM-NCWKWCPL'
+						height='0'
+						width='0'
+						style={{ display: 'none', visibility: 'hidden' }}
+					/>
+				</noscript>
 				<Marquee
 					items={itemsTopHeader}
 					mode='text'
